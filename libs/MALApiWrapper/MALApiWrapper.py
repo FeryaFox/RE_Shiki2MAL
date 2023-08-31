ebab4bf0c540805c7011dd5067b697d3b1ceb286
FeryaFox <bogdashkin145@gmail.com>
2023-09-01 00:37:42 +0300

    fix delete anime from MAL list

--- libs/MALApiWrapper/MALApiWrapper.py
+++ libs/MALApiWrapper/MALApiWrapper.py
@@ -75,7 +75,8 @@ class MALApiWrapper:
                 raise MALAddToListError(f"Error to add anime to list", r.status_code, r.json())
 
     def delete_anime_from_list(self, anime_id: int):
-
+        rewatch_value = 5 if rewatch_value is not None and rewatch_value > 5 else rewatch_value
+        print(rewatch_value)
         r = self.__fetch_api(
             f"https://api.myanimelist.net/v2/anime/{anime_id}/my_list_status",
             HttpMethod.delete
@@ -160,7 +161,7 @@ class MALApiWrapper:
                 return requests.put(url, data=kwargs, headers={"Authorization": f"Bearer {self.token.access_token}"})
 
             case HttpMethod.delete:
-                return requests.put(url, data=kwargs, headers={"Authorization": f"Bearer {self.token.access_token}"})
+                return requests.delete(url, data=kwargs, headers={"Authorization": f"Bearer {self.token.access_token}"})
 
             case HttpMethod.patch:
                 return requests.patch(url, data=kwargs, headers={"Authorization": f"Bearer {self.token.access_token}"})
